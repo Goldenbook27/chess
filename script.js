c2c462013ebf52dc9188dd43a7be08f62b80494b
Goldenbook27 <may.pranjali.naik@gmail.com>
2023-09-21 22:53:08 +0530

    new changes

--- script.js
+++ script.js
@@ -63,7 +63,60 @@ function dragOver(e){
 
 function dragDrop(e){
    e.stopPropagation()
+   console.log(e.target)
    const taken = e.target.classList.contains('piece')
-  //  e.target.parentNode.append(draggedElement)
-  //  e.target.append(draggedElement)
+   const valid = checkIfValid(e.target)
+   const correctGo = draggedElement.firstChild.classList.contains(playerGo)
+   const opponentGo = playerGo === 'white' ? 'black' : 'white'
+   const takenByOpponent = e.target.firstChild?.classList.contains(opponentGo)
+   if(correctGo){
+    if(takenByOpponent && valid){
+      e.target.parentNode.append(draggedElement)
+      e.target.remove()
+      changePlayer()
+      return
+    }
+   }
+  if(taken && !takenByOpponent){
+    infoDisplay.textContent = "You cannot go here!"
+    setTimeout(()=> infoDisplay.textContent = "",2000)
+    return
+  }
+  if(valid){
+    e.target.append(draggedElement)
+    changePlayer()
+    return
+  }
+}
+function changePlayer(){
+    if(playerGo==="black"){
+      reverseIds()
+      playerGo = "white"
+      playerDisplay.textContent="white"
+    }
+    else{
+      revertIds()
+      playerGo = "black"
+      playerDisplay.textContent="black"
+    }
+}
+
+function reverseIds(){
+  const allSquares = document.querySelectorAll('.square')
+  allSquares.forEach((square,i)=>
+       square.setAttribute('square-id',(width*width-1)-i)
+  )
+          
+}
+function revertIds(){
+  const allSquares = document.querySelectorAll('.square')
+  allSquares.forEach((square,i)=>
+       square.setAttribute('square-id',i)
+  )
+          
+}
+
+function checkIfValid(target){
+       console.log(target)
+       const targetId = target.getAttribute('square-id') || target.getAttribute
 }
